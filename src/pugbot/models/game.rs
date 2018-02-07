f00cdb3abc4be7d8bf02136b808aa25f662aa785
Mayberry RFD <m.attdeboard@gmail.com>
2018-02-07 17:49:36 -0600

    Add phase field to `Game`.
    This will help ensure operations are performed only at the appropriate points
    in the operational cycle of `Game`.

--- src/pugbot/models/game.rs
+++ src/pugbot/models/game.rs
@@ -2,9 +2,28 @@ use ::models::team::Team;
 use ::traits::pool_availability::PoolAvailability;
 use typemap::Key;
 
+enum Phase {
+  PlayerRegistration,
+  CaptainSelection,
+  PlayerDrafting,
+  MapSelection,
+  ResultRecording
+}
+
 pub struct Game<T: PoolAvailability> {
   pub teams: Option<Vec<Team>>,
-  pub draft_pool: T
+  pub draft_pool: T,
+  phase: Phase
+}
+
+impl<T> Game<T> where T: PoolAvailability {
+  fn new(teams: Option<Vec<Team>>, draft_pool: T) -> Game<T>{
+    Game {
+      teams: teams,
+      draft_pool: draft_pool,
+      phase: Phase::PlayerRegistration
+    }
+  }
 }
 
 impl<T> Key for Game<T> where T: 'static + PoolAvailability {
