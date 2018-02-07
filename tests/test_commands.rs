e78f7d338913b10ea2ba852e11703c90a3eb5001
Mayberry RFD <m.attdeboard@gmail.com>
2018-02-07 11:55:32 -0600

    Expand update_members test to test both open & closed draft pools.

--- tests/test_commands.rs
+++ tests/test_commands.rs
@@ -4,7 +4,8 @@ extern crate serde_json;
 extern crate serenity;
 
 use pugbot::commands;
-use pugbot::models;
+use pugbot::models::game::Game;
+use pugbot::models::draft_pool::DraftPool;
 use pugbot::traits;
 use serde::de::Deserialize;
 use serde_json::Value;
@@ -85,10 +86,19 @@ impl traits::pool_availability::PoolAvailability for ClosedPool {
 #[test]
 fn update_members() {
   let message = p!(Message, "message");
+
+  // Test updating members with a closed draft pool.
   let draft_pool = ClosedPool { members: Vec::new() };
-  let game = &mut models::game::Game { teams: None, draft_pool: draft_pool };
-  let users = commands::add::update_members(game, &message, false);
+  let game_closed = &mut Game { teams: None, draft_pool: draft_pool };
+  let users = commands::add::update_members(game_closed, &message, false);
   // There should be no members in the members vec, since `is_open` always yields
   // `false`.
   assert_eq!(users.len(), 0);
+
+  // Test updating members with an open draft pool.
+  let game_open = &mut Game { teams: None, draft_pool: DraftPool { members: Vec::new() } };
+  let users = commands::add::update_members(game_open, &message, false);
+  // There should be one member in the members vec, the author of the message (which is
+  // defined in ./resources/message.json)
+  assert_eq!(users.len(), 1);
 }
