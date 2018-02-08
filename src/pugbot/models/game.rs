66d0b86ad3c7b8c4e6bafb4a47d219f042a6b091
Mayberry RFD <m.attdeboard@gmail.com>
2018-02-07 18:26:52 -0600

    Add data structures to allow for moving up/down phases.

--- src/pugbot/models/game.rs
+++ src/pugbot/models/game.rs
@@ -1,6 +1,16 @@
 use ::models::team::Team;
 use ::traits::pool_availability::PoolAvailability;
+use std::collections::BTreeMap;
+use std::sync::{ Arc, Mutex };
 use typemap::Key;
+
+#[allow(dead_code)]
+pub struct Game<T: PoolAvailability> {
+  pub teams: Option<Vec<Team>>,
+  pub draft_pool: T,
+  phase_map: BTreeMap<i32, Phase>,
+  phase: Arc<Mutex<Phase>>
+}
 
 enum Phase {
   PlayerRegistration,
@@ -10,20 +20,25 @@ enum Phase {
   ResultRecording
 }
 
-pub struct Game<T: PoolAvailability> {
-  pub teams: Option<Vec<Team>>,
-  pub draft_pool: T,
-  phase: Phase
-}
-
 impl<T> Game<T> where T: PoolAvailability {
-  fn new(teams: Option<Vec<Team>>, draft_pool: T) -> Game<T>{
+  pub fn new(teams: Option<Vec<Team>>, draft_pool: T) -> Game<T>{
     Game {
       teams: teams,
       draft_pool: draft_pool,
-      phase: Phase::PlayerRegistration
+      phase_map: phase_map(),
+      phase: Arc::new(Mutex::new(Phase::PlayerRegistration))
     }
   }
+}
+
+fn phase_map() -> BTreeMap<i32, Phase> {
+  let mut phase_map = BTreeMap::new();
+  phase_map.insert(Phase::PlayerRegistration as i32, Phase::PlayerRegistration);
+  phase_map.insert(Phase::CaptainSelection as i32, Phase::CaptainSelection);
+  phase_map.insert(Phase::PlayerDrafting as i32, Phase::PlayerDrafting);
+  phase_map.insert(Phase::MapSelection as i32, Phase::MapSelection);
+  phase_map.insert(Phase::ResultRecording as i32, Phase::ResultRecording);
+  phase_map
 }
 
 impl<T> Key for Game<T> where T: 'static + PoolAvailability {
