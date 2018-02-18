e26ccb209966d47feb51838c7e4d5f1a72733a28
Mayberry RFD <m.attdeboard@gmail.com>
2018-02-17 19:11:41 -0600

    Flattne module.

--- src/pugbot/tables.rs
+++ src/pugbot/tables.rs
@@ -1,23 +1,18 @@
-pub mod users {
-  #[derive(Debug, Queryable, Identifiable, Associations)]
-  #[table_name = "users"]
-  #[primary_key(user_id)]
-  pub struct Users {
-    pub user_id: u64,
-    pub bot: bool,
-    pub discriminator: u16,
-    pub name: String
-  }
+#[table_name="users"]
+#[primary_key(user_id)]
+#[derive(Queryable, Insertable, Associations)]
+pub struct Users {
+  pub user_id: u64,
+  pub bot: bool,
+  pub discriminator: u16,
+  pub name: String
 }
 
-pub mod user_ratings {
-  #[belongs_to(tables::users::Users)]
-  #[table_name = "user_ratings"]
-  #[primary_key(user_rating_id)]
-  #[derive(Debug, Queryable, Identifiable, Associations)]
-  pub struct UserRatings {
-    pub user_rating_id: u64,
-    pub user_id: u64,
-    pub rating: f64
-  }
+#[table_name = "user_ratings"]
+#[derive(Queryable, Associations)]
+#[belongs_to(Users)]
+pub struct UserRatings {
+  pub id: u64,
+  pub user_id: u64,
+  pub rating: f64
 }
