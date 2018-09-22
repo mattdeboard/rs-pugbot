6ff03ee3ae82bbdeda924a1f1db81b34fd3777df
Matt DeBoard <matt.deboard@gmail.com>
2018-09-22 16:25:25 -0400

    Replace LawBreakers (RIP) with Rocket League.

--- migrations/2018-02-18-071905_add_default_games_and_modes/up.sql
+++ migrations/2018-02-18-071905_add_default_games_and_modes/up.sql
@@ -1,6 +1,6 @@
 insert into game_titles (game_name) values
   ('Overwatch'),
-  ('LawBreakers')
+  ('Rocket League')
 ;
 
 with ow_id as (
@@ -14,7 +14,13 @@ insert into game_modes (game_title_id, mode_name, team_size) values
   ((select id from ow_id), '1v1', 1),
   ((select id from ow_id), '5v5', 5);
 
-with lb_id as (select game_title_id as id from game_titles where game_name = 'LawBreakers')
+with rl_id as (
+  select game_title_id as id
+    from game_titles
+   where game_name = 'Rocket League'
+)
 insert into game_modes (game_title_id, mode_name, team_size) values
-  ((select id from lb_id), 'Standard', 5),
-  ((select id from lb_id), 'Team Deathmatch', 6);
+  ((select id from ow_id), 'Standard 3v3', 3),
+  ((select id from ow_id), '2v2', 2)
+  ((select id from ow_id), '4v4', 4),
+  ((select id from ow_id), '1v1', 1);
