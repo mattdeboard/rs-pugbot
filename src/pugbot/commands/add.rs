7b7fac669ea7efc2fada0ba7a1e53106806f711e
Matt DeBoard <matt.deboard@gmail.com>
2018-12-29 14:49:43 -0500

    Refactor tests so unit tests are in the right spots. (#26)

--- src/pugbot/commands/add.rs
+++ src/pugbot/commands/add.rs
@@ -37,3 +37,67 @@ pub fn update_members(
   game.next_phase();
   game.draft_pool.members()
 }
+
+#[cfg(test)]
+mod tests {
+  extern crate kankyo;
+  extern crate r2d2;
+  extern crate r2d2_diesel;
+  extern crate serde;
+  extern crate serde_json;
+  extern crate serenity;
+
+  use self::serde::de::Deserialize;
+  use self::serde_json::Value;
+  use commands;
+  use models::draft_pool::DraftPool;
+  use models::game::{Game, Phases};
+  use serenity::model::channel::Message;
+  use serenity::model::id::UserId;
+  use serenity::model::user::User;
+  use std::env;
+  use std::fs::File;
+
+  macro_rules! p {
+    ($s:ident, $filename:expr) => {{
+      let f =
+        File::open(concat!("./tests/resources/", $filename, ".json")).unwrap();
+      let v = serde_json::from_reader::<File, Value>(f).unwrap();
+
+      $s::deserialize(v).unwrap()
+    }};
+  }
+
+  fn gen_test_user() -> User {
+    User {
+      id: UserId(210),
+      avatar: Some("abc".to_string()),
+      bot: false,
+      discriminator: 1432,
+      name: "TestUser".to_string(),
+    }
+  }
+
+  #[test]
+  fn test_update_members() {
+    let message = p!(Message, "message");
+    let key = "TEAM_SIZE";
+    env::set_var(key, "1");
+    let game = &mut Game::new(
+      None,
+      DraftPool::new(vec![gen_test_user()]),
+      1,
+      Vec::new(),
+      // Draft pool max size: 12 (2 * 6)
+      2,
+      6,
+    );
+    assert_eq!(game.phase, Some(Phases::PlayerRegistration));
+    let members = commands::add::update_members(game, &message, false);
+    // There should be one member in the members vec to start with: our test user.
+    // `update_members` above should add an additional user, the author of the message (which is
+    // defined in ./resources/message.json).
+    assert_eq!(members.len(), 2);
+    assert_eq!(game.phase, Some(Phases::PlayerRegistration));
+  }
+}
