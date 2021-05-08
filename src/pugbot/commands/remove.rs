c2955a20479166b909058f4fa564171f6f08765f
Matt DeBoard <matt.deboard@gmail.com>
2021-05-08 17:54:45 -0500

    Convert command::remove::test to use tokio::test.

--- src/pugbot/commands/remove.rs
+++ src/pugbot/commands/remove.rs
@@ -25,8 +25,8 @@ pub async fn remove_member(
 ) -> Vec<User> {
   let mut data = ctx.data.write().await;
   let game = data.get_mut::<GameContainer>().unwrap();
-
   let author = msg.author.clone();
+
   if let Ok(_) = game.draft_pool.remove_member(&author) {
     let next_id = game.turn_taker.next().unwrap();
     if let Ok(_) = game.teams[next_id as usize].remove_member(&author) {
@@ -78,8 +78,9 @@ mod tests {
   use crate::{commands, struct_from_json};
   use serenity::model::channel::Message;
   use std::fs::File;
+  use tokio;
 
-  fn test_context(msg: &Message) -> Box<serenity::client::Context> {
+  async fn test_context(msg: &Message) -> serenity::client::Context {
     let context = commands::mock_context::tests::mock_context();
     {
       let game = Game::new(
@@ -91,30 +92,28 @@ mod tests {
         2,
         6,
       );
-      let mut data = tokio_test::block_on(context.data.write());
+      let mut data = context.data.write().await;
       data.insert::<GameContainer>(game);
     }
-    Box::new(context)
+    context
   }
 
-  #[allow(unused_must_use)]
-  #[test]
-  fn test_remove_member() {
+  // #[allow(unused_must_use)]
+  #[tokio::test]
+  async fn test_remove_member() {
     let message = struct_from_json!(Message, "message");
-    let context = test_context(&message);
-    let mut data = tokio_test::block_on(context.data.write());
+    let context = test_context(&message).await;
+    let mut data = context.data.write().await;
     let the_game = data.get_mut::<GameContainer>();
 
     if let Some(game) = the_game {
       assert_eq!(game.phase, Some(Phases::PlayerRegistration));
-      async {
-        // Precondition. Draft pool should have 1 member, the author of
-        // the message.
-        assert_eq!(game.draft_pool.members.len(), 1);
-        commands::remove::remove_member(&context, &message, false).await;
-        // Post condition. Pool should now be empty.
-        assert_eq!(game.draft_pool.members.len(), 0);
-      };
+      // Precondition. Draft pool should have 1 member, the author of
+      // the message.
+      assert_eq!(game.draft_pool.members.len(), 1);
+      let members = commands::remove::remove_member(&context, &message, false);
+      // Post condition. Pool should now be empty.
+      assert_eq!(members.await.len(), 0);
     }
   }
 }
