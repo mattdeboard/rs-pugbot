22b698fc2af710e2c0103ec8f59a5dfaa5f75b10
Matt DeBoard <matt.deboard@gmail.com>
2021-05-07 23:25:03 -0500

    Add async exec for main.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use kankyo;
 use pugbot;
+use tokio;
 
-fn main() {
+#[tokio::main(flavor = "multi_thread")]
+async fn main() {
   // This will load the environment variables located at `./.env`, relative to
   // the CWD. See `./.env.example` for an example on how to structure this.
   kankyo::load().expect("Failed to load .env file");
   // FIXME: needs an async executor to run
-  let _ = pugbot::client_setup();
+  pugbot::client_setup().await;
 }
